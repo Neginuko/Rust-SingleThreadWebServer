965ca2b7ac94d269844fbb02e885026d62aac369
Hayate Hirayama <20121061@kaishi-pu.ac.jp>
2023-03-03 12:54:56 +0900

    add 404

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,15 @@ fn handle_connection(mut stream: TcpStream) {
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
-        // some other handle
+        let status_line = "HTTP/1.1 404 Not Found\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
